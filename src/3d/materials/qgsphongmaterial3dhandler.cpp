23e40e436d29664d1dfc7616b54797411d064237
viperminiq <viperminiq@gmail.com>
2026-05-04 12:31:27 +0200

    srgb color phong

--- src/3d/materials/qgsphongmaterial3dhandler.cpp
+++ src/3d/materials/qgsphongmaterial3dhandler.cpp
@@ -288,31 +288,13 @@ QgsMaterial *QgsPhongMaterial3DHandler::buildMaterial( const QgsAbstractMaterial
 
   if ( !phongSettings->dataDefinedProperties().hasActiveProperties() )
   {
-    const QColor ambient = context.isSelected() ? context.selectionColor().darker() : phongSettings->ambient();
-    const QColor diffuse = context.isSelected() ? context.selectionColor() : phongSettings->diffuse();
-    const QColor specular = phongSettings->specular();
+    const QColor ambient = Qgs3DUtils::srgbToLinear( context.isSelected() ? context.selectionColor().darker() : phongSettings->ambient() );
+    const QColor diffuse = Qgs3DUtils::srgbToLinear( context.isSelected() ? context.selectionColor() : phongSettings->diffuse() );
+    const QColor specular = Qgs3DUtils::srgbToLinear( phongSettings->specular() );
 
-    material->setAmbient(
-      QColor::fromRgbF(
-        static_cast< float >( ambient.redF() * phongSettings->ambientCoefficient() ),
-        static_cast< float >( ambient.greenF() * phongSettings->ambientCoefficient() ),
-        static_cast< float >( ambient.blueF() * phongSettings->ambientCoefficient() )
-      )
-    );
-    material->setDiffuse(
-      QColor::fromRgbF(
-        static_cast< float >( diffuse.redF() * phongSettings->diffuseCoefficient() ),
-        static_cast< float >( diffuse.greenF() * phongSettings->diffuseCoefficient() ),
-        static_cast< float >( diffuse.blueF() * phongSettings->diffuseCoefficient() )
-      )
-    );
-    material->setSpecular(
-      QColor::fromRgbF(
-        static_cast< float >( specular.redF() * phongSettings->specularCoefficient() ),
-        static_cast< float >( specular.greenF() * phongSettings->specularCoefficient() ),
-        static_cast< float >( specular.blueF() * phongSettings->specularCoefficient() )
-      )
-    );
+    material->setAmbient( ambient );
+    material->setDiffuse( diffuse );
+    material->setSpecular( specular );
   }
 
   material->setShininess( static_cast<float>( phongSettings->shininess() ) );
